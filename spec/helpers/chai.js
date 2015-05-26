b9f1bd437f1793d0b8fb21ab7f37c4e86874d411
shyam vala <shyam.vala@gmail.com>
2015-05-26 00:16:24 -0400

    Fixing false positive

--- spec/helpers/chai.js
+++ spec/helpers/chai.js
@@ -1,7 +1,9 @@
 var chai = require('chai');
 
 chai.config.includeStack = true;
-
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
 global.expect = chai.expect;
 global.AssertionError = chai.AssertionError;
 global.Assertion = chai.Assertion;
--- spec/server/models/user.spec.js
+++ spec/server/models/user.spec.js
@@ -3,19 +3,21 @@ var helper = require("../../spec_helper")
 var db = require(helper.modelsPath)
 
 beforeEach(function(done) {
-  db.sequelize.sync({force: true});
-  done();
+  db.sequelize.sync({force: true})
+    .then(function() {
+      done();
+    });
 });
 
 describe("USER Model", function () {
 
   it("should create the user with a UUID", function(done) {
-    db.User
-      .create({email: "foo@test.com",password: "bar"})
-      .then(function(user) {
-        expect(user.uuid).not.to.be.null
-      });
-    done();
+    db.User.create({email: "foo@test.com",password: "bar"})
+      .should.be.fulfilled.then(function(user) {
+        expect(user.email).to.equal("foo@test.com")
+        expect(user.password).to.equal("bar")
+        expect(user.id).to.equal(1)
+      }).should.notify(done);
   });
 
 });
